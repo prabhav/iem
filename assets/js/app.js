4e7f7117769e853b5781424f6aae486669283383
Prabhav Khandelwal <me@prabhavkhandelwal.com>
2019-06-13 17:03:51 -0700

    metadata and starting sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,11 +6,11 @@ var main = function() {
 			$('nav ul').toggleClass('open');
 	});
 	// We listen to the resize event
-	window.addEventListener('resize', () => {
-		// We execute the same script as before
-		let vh = window.innerHeight * 0.01;
-		document.documentElement.style.setProperty('--vh', `${vh}px`);
-	});
+	// window.addEventListener('resize', () => {
+	// 	// We execute the same script as before
+	// 	let vh = window.innerHeight * 0.01;
+	// 	document.documentElement.style.setProperty('--vh', `${vh}px`);
+	// });
 };
 
 $(document).ready(main);
